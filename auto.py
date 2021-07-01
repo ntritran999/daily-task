69ad4b7a7a1a5bc73bce5b2581099e531ed6ea55
ntritran999 <81624097+ntritran999@users.noreply.github.com>
2021-07-01 13:06:35 +0700

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,26 @@
+import pyautogui, time, os
+
+# OPEN GAME
+pyautogui.click(712, 876)
+def click(pos):
+    time.sleep(10)
+    pyautogui.click(pos)
+
+# OPEN YOUTUBE
+def open_browser():
+    os.system('start firefox')
+    time.sleep(1)
+    pyautogui.click(324, 65)
+    pyautogui.typewrite('youtube')
+    pyautogui.typewrite(['enter'])
+
+positions = [(924, 575), (1027, 299), (694, 755)]
+
+def main():
+    for pos in positions:
+        click(pos)
+
+    open_browser()
+
+if __name__ == "__main__":
+    main()
