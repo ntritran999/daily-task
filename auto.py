36f6a3191e1dea691b38cd9692109710bdd86882
ntritran999 <81624097+ntritran999@users.noreply.github.com>
2021-07-05 12:11:25 +0700

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,25 +1,33 @@
-import pyautogui, time, os
-
-# OPEN GAME
-pyautogui.click(712, 876)
-def click(pos):
-    time.sleep(10)
-    pyautogui.click(pos)
+from pyautogui import click, typewrite
+from time import sleep
+import os
 
 # OPEN YOUTUBE
 def open_browser():
     os.system('start firefox')
-    time.sleep(1)
-    pyautogui.click(324, 65)
-    pyautogui.typewrite('youtube')
-    pyautogui.typewrite(['enter'])
+    sleep(3)
+    click(324, 65)
+    sleep(1)
+    typewrite('youtube')
+    typewrite(['enter'])
 
 def main():
+    # OPEN GAME
+    click(712, 876) # Choose garena
     positions = [(924, 575), (1027, 299), (694, 755)]
     for pos in positions:
+        sleep(10)
         click(pos)
 
     open_browser()
+
+    click(712, 876) 
+    sleep(2)
+    click(1213, 97) # Exit garena
+    sleep(2)
+    click(609, 878) # Choose vscode
+    sleep(1)
+    click(1424, 6) # Exit vscode
 
 if __name__ == "__main__":
     main()
